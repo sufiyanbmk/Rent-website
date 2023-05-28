76041df060408a1431b6234b32e8af4cb1541be9
sufiyanbmk <sufiyanbmk@gmail.com>
2023-05-28 14:24:33 +0530

    deleted backend

--- backend/routes/report.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createReport } from '../controllers/reportController.js';
-
-const router = express.Router()
-
-router.post('/:proId', createReport )
-
-export default router;
--- backend/routes/review.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createReview } from '../controllers/reviewController.js';
-
-const router = express.Router()
-
-router.post('/:proId', createReview )
-
-export default router;
