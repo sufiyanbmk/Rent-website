614f8d5fc49e470236b4e12a19c925e42f382fb2
sufiyanbmk <sufiyanbmk@gmail.com>
2024-06-26 01:12:15 +0400

    chore: changed base url

--- frondend/src/Axios/axios.js
+++ frondend/src/Axios/axios.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 const token = localStorage.getItem('access_token');
+// https://api.rentit.fun/api
+// prettier-ignore
+const baseURL = process.env.NODE_ENV === 'production' ? '/api' : 'http://13.201.117.19/api';
+
 const instance = axios.create({
-  baseURL: 'https://api.rentit.fun/api',
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${token}`,
